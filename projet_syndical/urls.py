3abad75c1a83030c1d4d2a8d9f8187e0aabba567
Mousto <bediaraye-w@yahoo.fr>
2022-08-31 01:16:23 +0200

    modif syndicat_api/views.py: ajout CustumUserCreate, modif syndicat_api/serializers.py:RegisterUserSerializer, projet_syndical/urls.py: ajout accesToken et refreshToken

--- projet_syndical/urls.py
+++ projet_syndical/urls.py
@@ -32,5 +32,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api-token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- syndicat_api/serializers.py
+++ syndicat_api/serializers.py
@@ -1,7 +1,26 @@
 from dataclasses import fields
 from pyexpat import model
 from rest_framework import serializers
-from syndicat.models import Produit, Commande, DoleanceElu, Info
+from syndicat.models import Produit, Commande, DoleanceElu, Info, Personnel
+
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Personnel
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+
+
 
 class ProduitSerializer(serializers.ModelSerializer):
     class Meta:
